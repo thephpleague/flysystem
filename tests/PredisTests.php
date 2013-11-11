5552690a126c5d742b9879ff6b2d5b914653f013
Frank de Jonge <info@frenky.net>
2013-11-11 16:20:07 +0100

    Added predis tests

---
+++ tests/PredisTests.php
@@ -0,0 +1,45 @@
+<?php
+
+use Flysystem\Cache\Predis;
+use Predis\Client;
+
+class PredisTests extends PHPUnit_Framework_TestCase
+{
+    public function testLoadFail()
+    {
+        $client = Mockery::mock('Predis\Client');
+        $client->shouldReceive('get')->once()->andReturn(null);
+        $cache = new Predis($client);
+        $cache->load();
+        $this->assertFalse($cache->isComplete());
+    }
+
+    public function testLoadSuccess()
+    {
+        $response = json_encode([true, []]);
+        $client = Mockery::mock('Predis\Client');
+        $client->shouldReceive('get')->once()->andReturn($response);
+        $cache = new Predis($client);
+        $cache->load();
+        $this->assertTrue($cache->isComplete());
+    }
+
+    public function testSave()
+    {
+        $response = json_encode([false, []]);
+        $client = Mockery::mock('Predis\Client');
+        $client->shouldReceive('set')->once()->andReturn($response);
+        $cache = new Predis($client);
+        $cache->save();
+    }
+
+    public function testSaveWithExpire()
+    {
+        $response = json_encode([false, []]);
+        $client = Mockery::mock('Predis\Client');
+        $client->shouldReceive('set')->once()->andReturn($response);
+        $client->shouldReceive('expire')->once();
+        $cache = new Predis($client, 'flysystem', 20);
+        $cache->save();
+    }
+}
