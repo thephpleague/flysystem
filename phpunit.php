6fa4de7c6e60c2c7c1573fe620d40e562268c9f3
Frank de Jonge <info@frenky.net>
2013-11-11 20:54:32 +0100

    Ensure test dir for local testing on a fresh clone

--- phpunit.php
+++ phpunit.php
@@ -2,6 +2,10 @@
 
 include __DIR__.'/vendor/autoload.php';
 
+if ( ! is_dir(__DIR__.'/tests/files')) {
+    mkdir(__DIR__.'/tests/files', 0777, true);
+}
+
 $fs = new Flysystem\Adapter\Local(__DIR__.'/tests/files');
 foreach (array_reverse($fs->listContents()) as $info) {
 	if (is_file(__DIR__.'/tests/files/'.$info['path'])) {
