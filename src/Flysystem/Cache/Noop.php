f358854f14354462d63372c79f303b3b9f754e4f
Frank de Jonge <info@frenky.net>
2013-12-02 10:13:33 +0100

    Docblocks for Flysystem\Cache\Noop and Directory handler

--- src/Flysystem/Cache/Noop.php
+++ src/Flysystem/Cache/Noop.php
@@ -4,88 +4,142 @@ namespace Flysystem\Cache;
 
 class Noop extends AbstractCache
 {
+    /**
+     * {@inheritdoc}
+     */
     protected $autosave = false;
 
+    /**
+     * {@inheritdoc}
+     */
     public function updateObject($path, array $object, $autosave = false)
     {
         return $object;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function isComplete($dirname, $recursive)
     {
         return false;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setComplete($dirname, $recursive)
     {
 
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function storeContents($directory, array $contents, $recursive)
     {
         return $contents;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function flush()
     {
 
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function autosave()
     {
 
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function save()
     {
 
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function load()
     {
 
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function has($path)
     {
         return null;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function read($path)
     {
         return false;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function listContents($directory = '', $recursive = false)
     {
         return false;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getMetadata($path)
     {
         return false;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getSize($path)
     {
         return false;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getMimetype($path)
     {
         return false;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getTimestamp($path)
     {
         return false;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getVisibility($path)
     {
         return false;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function ensureParentDirectories($path)
     {
         return false;
--- src/Flysystem/Directory.php
+++ src/Flysystem/Directory.php
@@ -4,11 +4,22 @@ namespace Flysystem;
 
 class Directory extends Handler
 {
+    /**
+     * Delete the directory
+     *
+     * @return  void
+     */
     public function delete()
     {
         $this->filesystem->deleteDir($this->path);
     }
 
+    /**
+     * List the directory contents
+     *
+     * @param   boolean        $recursive
+     * @return  array|boolean  directort contents or false
+     */
     public function getContents($recursive = false)
     {
         return $this->filesystem->listContents($this->path, $recursive);
