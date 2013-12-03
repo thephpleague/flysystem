a827f620b7a1e3f91ec41a58618feeedbbf9c231
Frank de Jonge <info@frenky.net>
2013-12-03 22:20:29 +0100

    Added docblocks to File handler

--- src/Flysystem/File.php
+++ src/Flysystem/File.php
@@ -4,28 +4,53 @@ namespace Flysystem;
 
 class File extends Handler
 {
+    /**
+     * Read the file
+     *
+     * @return  string  file contents
+     */
     public function read()
     {
         return $this->filesystem->read($this->path);
     }
 
+    /**
+     * Get the file's timestamp
+     *
+     * @return  int  unix timestamp
+     */
     public function getTimestamp()
     {
         return $this->filesystem->getTimestamp($this->path);
     }
 
+    /**
+     * Get the file's mimetype
+     *
+     * @return  string  mimetime
+     */
     public function getMimetype()
     {
         return $this->filesystem->getMimetype($this->path);
     }
 
+    /**
+     * Get the file size
+     *
+     * @return  int  file size
+     */
     public function getSize()
     {
         return $this->filesystem->getSize($this->path);
     }
 
+    /**
+     * Delete the file
+     *
+     * @return  boolean  success boolean
+     */
     public function delete()
     {
-        $this->filesystem->delete($this->path);
+        return $this->filesystem->delete($this->path);
     }
 }
