29a8597474c227abc8d51c97885ba3cc140669b8
Frank de Jonge <info@frenky.net>
2013-12-03 22:16:20 +0100

    Added docblocks to FileNotFoundException

--- src/Flysystem/FileNotFoundException.php
+++ src/Flysystem/FileNotFoundException.php
@@ -7,14 +7,29 @@ use LogicException;
 
 class FileNotFoundException extends LogicException
 {
+    /**
+     * @var  string  $path
+     */
     protected $path;
 
+    /**
+     * Constructor
+     *
+     * @param  string     $path
+     * @param  int        $code
+     * @param  Exception  $previous
+     */
     public function __construct($path, $code = 0, Exception $previous = null)
     {
         $this->path = $path;
         parent::__construct('File not found at path: '.$this->getPath(), $code, $previous);
     }
 
+    /**
+     * Get the path which was not found
+     *
+     * @return  string
+     */
     public function getPath()
     {
         return $this->path;
