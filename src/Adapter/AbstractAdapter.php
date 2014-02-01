572a3b04d791dc424f1ca320643de9fedba6ee2d
Frank de Jonge <info@frenky.net>
2014-02-01 21:18:04 +0100

    Added docblocks for AbstractAdapter.

--- src/Adapter/AbstractAdapter.php
+++ src/Adapter/AbstractAdapter.php
@@ -7,16 +7,37 @@ use League\Flysystem\AdapterInterface;
 
 abstract class AbstractAdapter implements AdapterInterface
 {
+    /**
+     * Write using a stream
+     *
+     * @param   string  $path
+     * @param   resource  $resource
+     * @param   mixed     $config
+     * @return  mixed     false or file metadata
+     */
     public function writeStream($path, $resource, $config = null)
     {
         return $this->stream($path, $resource, $config, 'write');
     }
 
+    /**
+     * Update a file using a stream
+     *
+     * @param   string    $path
+     * @param   resource  $resource
+     * @return  mixed     false of file metadata
+     */
     public function updateStream($path, $resource)
     {
         return $this->stream($path, $resource, null, 'update');
     }
 
+    /**
+     * Get the contents of a file in a stream
+     *
+     * @param   string          $path
+     * @return  resource|false  false when not found, or a resource
+     */
     public function readStream($path)
     {
         if ( ! $data = $this->read($path)) {
@@ -32,6 +53,15 @@ abstract class AbstractAdapter implements AdapterInterface
         return $data;
     }
 
+    /**
+     * Stream fallback
+     *
+     * @param   string    $path
+     * @param   resource  $resource
+     * @param   mixed     $config
+     * @param   string    $fallback
+     * @return  mixed     fallback result
+     */
     protected function stream($path, $resource, $config, $fallback)
     {
         rewind($resource);
@@ -40,11 +70,24 @@ abstract class AbstractAdapter implements AdapterInterface
         return $this->{$fallback}($path, $contents, $config);
     }
 
+    /**
+     * Get the file visibility
+     *
+     * @param   string  $path
+     * @throws  LogicException
+     */
     public function getVisibility($path)
     {
         throw new LogicException(get_class($this).' does not support visibility settings.');
     }
 
+    /**
+     * Set the file visibility
+     *
+     * @param   string  $path
+     * @param   string  $visibility
+     * @throws  LogicException
+     */
     public function setVisibility($path, $visibility)
     {
         throw new LogicException(get_class($this).' does not support visibility settings.');
