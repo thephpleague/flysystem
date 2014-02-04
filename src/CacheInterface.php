fe63d2d6a55b70aad95a8f10208180e51d78abe8
Frank de Jonge <info@frenky.net>
2014-02-04 09:40:53 +0100

    More interface docblocks, this should stop @rdohms from telling on me to @mvriel.

--- src/CacheInterface.php
+++ src/CacheInterface.php
@@ -4,11 +4,58 @@ namespace League\Flysystem;
 
 interface CacheInterface extends ReadInterface
 {
+    /**
+     * Check wether the directory listing of a given directory is complete
+     *
+     * @param   path  $dirname
+     * @param   bool  $recursive
+     * @return  bool
+     */
     public function isComplete($dirname, $recursive);
+
+    /**
+     * Set a directory to completely listed
+     *
+     * @param  path  $dirname
+     * @param  bool  $recursive
+     */
     public function setComplete($dirname, $recursive);
+
+    /**
+     * Store the contents of a directory
+     *
+     * @param   path   $directory
+     * @param   array  $contents
+     * @param   bool   $recursive
+     * @return  array  contents
+     */
     public function storeContents($directory, array $contents, $recursive);
+
+    /**
+     * Flush the cache
+     *
+     * @return  void
+     */
     public function flush();
+
+    /**
+     * Autosave trigger
+     *
+     * @return  void
+     */
     public function autosave();
+
+    /**
+     * Store the cache
+     *
+     * @return  void
+     */
     public function save();
+
+    /**
+     * Load the cache
+     *
+     * @return  void
+     */
     public function load();
 }
--- src/ReadInterface.php
+++ src/ReadInterface.php
@@ -4,13 +4,76 @@ namespace League\Flysystem;
 
 interface ReadInterface
 {
+    /**
+     * Check wether a file exists
+     *
+     * @param   string  $path
+     * @return  bool
+     */
     public function has($path);
+
+    /**
+     * Read a file
+     *
+     * @param   string  $path
+     * @return  false|array
+     */
     public function read($path);
+
+    /**
+     * Read a file as a stream
+     *
+     * @param   string  $path
+     * @return  false|array
+     */
     public function readStream($path);
+
+    /**
+     * List contents of a directory
+     *
+     * @param   string  $directory
+     * @param   bool    $recursive
+     * @return  false|array
+     */
     public function listContents($directory = '', $recursive = false);
+
+    /**
+     * Get all the meta data of a file or directory
+     *
+     * @param   string  $path
+     * @return  false|array
+     */
     public function getMetadata($path);
+
+    /**
+     * Get all the meta data of a file or directory
+     *
+     * @param   string  $path
+     * @return  false|array
+     */
     public function getSize($path);
+
+    /**
+     * Get the mimetype of a file
+     *
+     * @param   string  $path
+     * @return  false|array
+     */
     public function getMimetype($path);
+
+    /**
+     * Get the timestamp of a file
+     *
+     * @param   string  $path
+     * @return  false|array
+     */
     public function getTimestamp($path);
+
+    /**
+     * Get the visibility of a file
+     *
+     * @param   string  $path
+     * @return  false|array
+     */
     public function getVisibility($path);
 }
