be3d32e1074182257d6fc1126ae46e376d8b3427
Graham Campbell <graham@mineuk.com>
2014-02-16 20:06:13 +0000

    Added new cache adapter

---
+++ src/Cache/Adapter.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace League\Flysystem\Cache;
+
+use League\Flysystem\AdapterInterface;
+
+class Adapter extends AbstractCache
+{
+    /**
+     * @var  \League\Flysystem\AdapterInterface  $adapter  An adapter
+     */
+    protected $adapter;
+
+    /**
+     * @var  string  $key  storage key
+     */
+    protected $key;
+
+    /**
+     * @var  int|null  $expire  seconds until cache expiration
+     */
+    protected $expire = null;
+
+    /**
+     * Constructor
+     *
+     * @param \League\Flysystem\AdapterInterface  $adapter  adapter
+     * @param string                              $key      storage key
+     * @param int|null                            $expire   seconds until cache expiration
+     */
+    public function __construct(AdapterInterface $adapter, $key = 'flysystem', $expire = null)
+    {
+        $this->adapter = $adapter;
+        $this->key = $key;
+        if ($expire) {
+            $this->expire = getTime($expire);
+        }
+    }
+
+    /**
+     * Get expiration time in seconds
+     *
+     * @param  int  $time  relative expiration time
+     * @return int  actual expiration time
+     */
+    protected function getTime($time = 0)
+    {
+        return intval(microtime(true)) + $time;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setFromStorage($json)
+    {
+        list ($cache, $complete, $expire) = json_decode($json, true);
+
+        if ( ! $expire || $expire > $this->getTime()) {
+            $this->cache = $cache;
+            $this->complete = $complete;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load()
+    {
+        $contents = $this->adapter->get($this->key);
+
+        if ($contents) {
+            $this->setFromStorage($contents);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getForStorage()
+    {
+        $cleaned = $this->cleanContents($this->cache);
+
+        return json_encode(array($cleaned, $this->complete, $this->expire));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function save()
+    {
+        $contents = $this->getForStorage();
+
+        $this->adapter->write($this->key, $contents);
+    }
+}
