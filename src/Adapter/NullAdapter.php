10e9c19f12c56edb9af17f651c40eeb5d1e70de6
Frank de Jonge <info@frenky.net>
2014-04-09 16:32:29 +0200

    Added NullAdapter

---
+++ src/Adapter/NullAdapter.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace League\Flysystem\Adapter;
+
+use League\Flysystem\Util;
+
+class NullAdapter extends AbstractAdapter
+{
+    /**
+     * Check whether a file is present
+     *
+     * @param   string   $path
+     * @return  boolean
+     */
+    public function has($path)
+    {
+        return false;
+    }
+
+    /**
+     * Write a file
+     *
+     * @param $path
+     * @param $contents
+     * @param null $config
+     * @return array|bool
+     */
+    public function write($path, $contents, $config = null)
+    {
+        $type = 'file';
+        $config = Util::ensureConfig($config);
+        $result = compact('contents', 'type', 'size',  'path');
+
+        if ($visibility = $config->get('visibility')) {
+            $result['visibility'] = $visibility;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Update a file
+     *
+     * @param   string       $path
+     * @param   string       $contents
+     * @param   mixed        $config   Config object or visibility setting
+     * @return  array|bool
+     */
+    public function update($path, $contents, $config = null)
+    {
+        return false;
+    }
+
+    /**
+     * Read a file
+     *
+     * @param   string  $path
+     * @return  array|bool
+     */
+    public function read($path)
+    {
+        return false;
+    }
+
+    /**
+     * Rename a file
+     *
+     * @param $path
+     * @param $newpath
+     * @return bool
+     */
+    public function rename($path, $newpath)
+    {
+        return false;
+    }
+
+
+    /**
+     * Delete a file
+     *
+     * @param $path
+     * @return bool
+     */
+    public function delete($path)
+    {
+        return false;
+    }
+
+    /**
+     * List contents of a directory
+     *
+     * @param string $directory
+     * @param bool $recursive
+     * @return array
+     */
+    public function listContents($directory = '', $recursive = false)
+    {
+        return false;
+    }
+
+    /**
+     * Get the metadata of a file
+     *
+     * @param $path
+     * @return array|false
+     */
+    public function getMetadata($path)
+    {
+        return false;
+    }
+
+    /**
+     * Get the size of a file
+     *
+     * @param $path
+     * @return array|false
+     */
+    public function getSize($path)
+    {
+        return false;
+    }
+
+    /**
+     * Get the mimetype of a file
+     *
+     * @param $path
+     * @return array
+     */
+    public function getMimetype($path)
+    {
+        return false;
+    }
+
+    /**
+     * Get the timestamp of a file
+     *
+     * @param $path
+     * @return array|boolean
+     */
+    public function getTimestamp($path)
+    {
+        return false;
+    }
+
+    /**
+     * Get the visibility of a file
+     *
+     * @param $path
+     * @return array|bool
+     */
+    public function getVisibility($path)
+    {
+        return false;
+    }
+
+    /**
+     * Set the visibility of a file
+     *
+     * @param $path
+     * @param $visibility
+     * @return array|void
+     */
+    public function setVisibility($path, $visibility)
+    {
+        return compact('visibility');
+    }
+
+    /**
+     * Create a directory
+     *
+     * @param $dirname
+     * @return array
+     */
+    public function createDir($dirname)
+    {
+        return array('path' => $dirname, 'type' => 'dir');
+    }
+
+    /**
+     * Delete a directory
+     *
+     * @param $dirname
+     * @return bool
+     */
+    public function deleteDir($dirname)
+    {
+        return false;
+    }
+}
---
+++ tests/NullAdapterTests.php
@@ -0,0 +1,85 @@
+<?php
+
+use League\Flysystem\Adapter\NullAdapter;
+use League\Flysystem\Filesystem;
+
+class NullAdapterTest extends PHPUnit_Framework_TestCase
+{
+    protected function getFilesystem()
+    {
+        return new Filesystem(new NullAdapter);
+    }
+
+    public function testWrite()
+    {
+        $fs = $this->getFilesystem();
+        $result = $fs->write('path', 'contents');
+        $this->assertTrue($result);
+        $this->assertTrue($fs->has('path'));
+    }
+
+    /**
+     * @expectedException  \League\Flysystem\FileNotFoundException
+     */
+    public function testRead()
+    {
+        $fs = $this->getFilesystem();
+        $fs->read('something');
+    }
+
+    public function testHas()
+    {
+        $fs = $this->getFilesystem();
+        $this->assertFalse($fs->has('something'));
+    }
+
+    public function testDelete()
+    {
+        $fs = $this->getFilesystem();
+        $this->assertFalse($fs->has('something'));
+    }
+
+    public function expectedFailsProvider()
+    {
+        return array(
+            array('read'),
+            array('update'),
+            array('read'),
+            array('rename'),
+            array('listContents'),
+            array('getMetadata'),
+            array('getSize'),
+            array('getMimetype'),
+            array('getTimestamp'),
+            array('getVisibility'),
+            array('deleteDir'),
+        );
+    }
+
+    /**
+     * @dataProvider expectedFailsProvider
+     */
+    public function testExpectedFails($method)
+    {
+        $adapter = new NullAdapter;
+        $this->assertFalse($adapter->{$method}('one', 'two', 'three'));
+    }
+
+    public function expectedArrayResultProvider()
+    {
+        return array(
+            array('write'),
+            array('createDir'),
+            array('setVisibility'),
+        );
+    }
+
+    /**
+     * @dataProvider expectedArrayResultProvider
+     */
+    public function testArrayResult($method)
+    {
+        $adapter = new NullAdapter;
+        $this->assertInternalType('array', $adapter->{$method}('one', tmpfile(), ['visibility' => 'public']));
+    }
+}
