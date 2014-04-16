226be25d7ee4f802cc02ff536ef271aa4d319c04
Frank de Jonge <info@frenky.net>
2014-04-16 22:59:16 +0200

    Tested setPrefix method

--- tests/RackspaceTests.php
+++ tests/RackspaceTests.php
@@ -39,6 +39,8 @@ class RackspaceTests extends PHPUnit_Framework_TestCase
         $container->shouldReceive('getObject')->andReturn($dataObject);
         $adapter = new Rackspace($container, 'prefix');
         $this->assertInternalType('array', $adapter->read('filename.ext'));
+        $adapter->setPrefix('/');
+        $this->assertEquals('path.ext', $adapter->prefix('path.ext'));
     }
 
     public function testRead()
