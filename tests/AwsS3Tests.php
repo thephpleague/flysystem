67872b609bed429a42283b8059ecfe341cdac174
Frank de Jonge <info@frenky.net>
2014-06-04 01:04:12 +0200

    5.3 style arrays, such fun

--- tests/AwsS3Tests.php
+++ tests/AwsS3Tests.php
@@ -88,7 +88,7 @@ class AwsS3Tests extends PHPUnit_Framework_TestCase
                 'visibility' => 'private',
                 'mimetype'   => 'text/plain',
                 'Expires'    => 'it does',
-                'Metadata' => [],
+                'Metadata' => array(),
             )
         );
     }
