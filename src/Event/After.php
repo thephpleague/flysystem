ae0cfc071075c2d26621dc0610f94f861228675d
Frank de Jonge <info@frenky.net>
2014-06-24 16:38:51 +0200

    Initial EventableFilesystem

---
+++ src/Event/After.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FrenkyNet
+ * Date: 23/06/14
+ * Time: 14:52
+ */
+
+namespace League\Flysystem\Event;
+
+use League\Event\EventAbstract;
+use League\Flysystem\FilesystemInterface;
+
+
+class After extends EventAbstract
+{
+    protected $filesystem;
+
+    protected $method;
+
+    protected $result;
+
+    /**
+     * @param FilesystemInterface $filesystem
+     * @param $method
+     * @param mixed $result
+     */
+    public function __construct(FilesystemInterface $filesystem, $method, $result)
+    {
+        $this->filesystem = $filesystem;
+        $this->method = $method;
+        $this->result = $result;
+    }
+
+    /**
+     * Get the Filesystem instance
+     *
+     * @return FilesystemInterface
+     */
+    public function getFilesystem()
+    {
+        return $this->filesystem;
+    }
+
+
+    /**
+     * Get the event name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        $method = $this->getMethod();
+
+        return 'after.' . strtolower($method);
+    }
+
+    /**
+     * Get the called method
+     *
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * Get the method call result
+     *
+     * @return mixed
+     */
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+    /**
+     * Overwrite the result
+     *
+     * @param mixed $result
+     * @return $this
+     */
+    public function setResult($result)
+    {
+        $this->result = $result;
+
+        return $this;
+    }
+}
