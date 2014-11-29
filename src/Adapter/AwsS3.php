6c1a20e4185875ea34ef51f411a6f6825f312e3a
Younès El Biache <younes.elbiache@gmail.com>
2014-11-29 11:11:11 +0100

    aws3 adapter request params

--- src/Adapter/AwsS3.php
+++ src/Adapter/AwsS3.php
@@ -36,6 +36,9 @@ class AwsS3 extends AbstractAdapter
         'Metadata',
         'ACL',
         'ContentType'
+        'ContentDisposition',
+        'ContentLanguage',
+        'ContentEncoding',
     );
 
     /**
