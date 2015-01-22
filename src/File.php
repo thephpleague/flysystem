787783a772ea781db72a44d45ff14c89d4f33fa0
Carson Full <carson.full@gmomail.org>
2015-01-21 22:16:58 -0600

    Added more methods to File class. Tests included.

--- src/File.php
+++ src/File.php
@@ -5,6 +5,16 @@ namespace League\Flysystem;
 class File extends Handler
 {
     /**
+     * Check whether the file exists.
+     *
+     * @return bool
+     */
+    public function exists()
+    {
+        return $this->filesystem->has($this->path);
+    }
+
+    /**
      * Read the file.
      *
      * @return string file contents
@@ -22,6 +32,30 @@ class File extends Handler
     public function readStream()
     {
         return $this->filesystem->readStream($this->path);
+    }
+
+    /**
+     * Write the new file.
+     *
+     * @param string $content
+     *
+     * @return bool success boolean
+     */
+    public function write($content)
+    {
+        return $this->filesystem->write($this->path, $content);
+    }
+
+    /**
+     * Write the new file using a stream.
+     *
+     * @param resource $resource
+     *
+     * @return bool success boolean
+     */
+    public function writeStream($resource)
+    {
+        return $this->filesystem->writeStream($this->path, $resource);
     }
 
     /**
@@ -46,6 +80,61 @@ class File extends Handler
     public function updateStream($resource)
     {
         return $this->filesystem->updateStream($this->path, $resource);
+    }
+
+    /**
+     * Create the file or update if exists.
+     *
+     * @param string $content
+     *
+     * @return bool success boolean
+     */
+    public function put($content)
+    {
+        return $this->filesystem->put($this->path, $content);
+    }
+
+    /**
+     * Create the file or update if exists using a stream.
+     *
+     * @param resource $resource
+     *
+     * @return bool success boolean
+     */
+    public function putStream($resource)
+    {
+        return $this->filesystem->putStream($this->path, $resource);
+    }
+
+    /**
+     * Rename the file.
+     *
+     * @param string $newpath
+     *
+     * @return bool success boolean
+     */
+    public function rename($newpath)
+    {
+        if ($this->filesystem->rename($this->path, $newpath)) {
+            $this->path = $newpath;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Copy the file.
+     *
+     * @param string $newpath
+     *
+     * @return File|false new file or false
+     */
+    public function copy($newpath)
+    {
+        if ($this->filesystem->copy($this->path, $newpath)) {
+            return new File($this->filesystem, $newpath);
+        }
+        return false;
     }
 
     /**
--- tests/FileTests.php
+++ tests/FileTests.php
@@ -4,6 +4,7 @@ namespace League\Flysystem;
 
 class FileTests extends \PHPUnit_Framework_TestCase
 {
+    /** @var Filesystem */
     protected $filesystem;
 
     public function setup()
@@ -18,13 +19,24 @@ class FileTests extends \PHPUnit_Framework_TestCase
 
     public function tearDown()
     {
-        $this->filesystem->delete('file.txt');
+        try {
+            $this->filesystem->delete('file.txt');
+        } catch (FileNotFoundException $e) { }
         $this->filesystem->deleteDir('files');
     }
 
+    /**
+     * @return File
+     */
     protected function getFile()
     {
         return $this->filesystem->get('file.txt');
+    }
+
+    public function testExists()
+    {
+        $file = $this->getFile();
+        $this->assertTrue($file->exists());
     }
 
     public function testRead()
@@ -38,6 +50,24 @@ class FileTests extends \PHPUnit_Framework_TestCase
     {
         $file = $this->getFile();
         $this->assertInternalType('resource', $file->readStream());
+    }
+
+    public function testWrite()
+    {
+        $file = new File();
+        $this->filesystem->get('files/new.txt', $file);
+        $file->write('new contents');
+        $this->assertEquals('new contents', $file->read());
+    }
+
+    public function testWriteStream()
+    {
+        $file = new File();
+        $this->filesystem->get('files/new.txt', $file);
+        $resource = tmpfile();
+        fwrite($resource, 'stream contents');
+        $file->writeStream($resource);
+        $this->assertEquals('stream contents', $file->read());
     }
 
     public function testUpdate()
@@ -55,5 +85,54 @@ class FileTests extends \PHPUnit_Framework_TestCase
         $file->updateStream($resource);
         fclose($resource);
         $this->assertEquals('stream contents', $file->read());
+    }
+
+    public function testPut()
+    {
+        $file = new File();
+        $this->filesystem->get('files/new.txt', $file);
+        $file->put('new contents');
+        $this->assertEquals('new contents', $file->read());
+        $file->put('updated content');
+        $this->assertEquals('updated content', $file->read());
+    }
+
+    public function testPutStream()
+    {
+        $file = new File();
+        $this->filesystem->get('files/new.txt', $file);
+
+        $resource = tmpfile();
+        fwrite($resource, 'stream contents');
+        $file->putStream($resource);
+        fclose($resource);
+
+        $this->assertEquals('stream contents', $file->read());
+
+        $resource = tmpfile();
+        fwrite($resource, 'updated stream contents');
+        $file->putStream($resource);
+        fclose($resource);
+
+        $this->assertEquals('updated stream contents', $file->read());
+    }
+
+    public function testRename()
+    {
+        $file = $this->getFile();
+        $file->rename('files/renamed.txt');
+        $this->assertFalse($this->filesystem->has('file.txt'));
+        $this->assertTrue($this->filesystem->has('files/renamed.txt'));
+        $this->assertEquals('files/renamed.txt', $file->getPath());
+    }
+
+    public function testCopy()
+    {
+        $file = $this->getFile();
+        $copied = $file->copy('files/copied.txt');
+        $this->assertTrue($this->filesystem->has('file.txt'));
+        $this->assertTrue($this->filesystem->has('files/copied.txt'));
+        $this->assertEquals('file.txt', $file->getPath());
+        $this->assertEquals('files/copied.txt', $copied->getPath());
     }
 }
