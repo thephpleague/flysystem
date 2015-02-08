27971dfa5444f06e4ab2a609b061164fae65ae6e
Naoki Sawada <hypweb@gmail.com>
2015-02-09 01:14:38 +0900

    `getAdapter()` support recursion type adapter
    Like CachedAdapter.
    
    ref. #412

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -46,7 +46,11 @@ class Filesystem implements FilesystemInterface
      */
     public function getAdapter()
     {
-        return $this->adapter;
+        if (method_exists($this->adapter, 'getAdapter')) {
+            return $this->adapter->getAdapter();
+        } else {
+            return $this->adapter;
+        }
     }
 
     /**
