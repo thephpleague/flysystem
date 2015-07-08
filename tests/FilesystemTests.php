738749afc09913f5ac141dd28cc0c1e7d986553b
frankdejonge <info@frenky.net>
2015-07-08 14:34:52 +0200

    PHP 7 and phpspec had some issues for calls with different argument signatures.

--- tests/FilesystemTests.php
+++ tests/FilesystemTests.php
@@ -203,8 +203,7 @@ class FilesystemTests extends ProphecyTestCase
     {
         $old = 'old.txt';
         $new = 'new.txt';
-        $this->prophecy->has($old)->willReturn(true);
-        $this->prophecy->has($new)->willReturn(false);
+        $this->prophecy->has(Argument::any())->willReturn(true, false);
         $this->prophecy->rename($old, $new)->willReturn(true);
         $response = $this->filesystem->rename($old, $new);
         $this->assertTrue($response);
