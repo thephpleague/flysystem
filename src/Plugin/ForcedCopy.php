a36f2b064378fad6ae1abba9e51c7a74f45e1646
Chris Leppanen <chris.leppanen@gmail.com>
2016-06-03 15:58:16 -0700

    Implement copy and rename plugins that overwrite destination.

---
+++ src/Plugin/ForcedCopy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace League\Flysystem\Plugin;
+
+use League\Flysystem\FileNotFoundException;
+
+class ForcedCopy extends AbstractPlugin
+{
+    /**
+     * @inheritdoc
+     */
+    public function getMethod()
+    {
+        return 'forceCopy';
+    }
+
+    /**
+     * Copies a file, overwriting any existing files.
+     *
+     * @param string $path    Path to the existing file.
+     * @param string $newpath The new path of the file.
+     *
+     * @throws FileNotFoundException Thrown if $path does not exist.
+     *
+     * @return bool True on success, false on failure.
+     */
+    public function handle($path, $newpath)
+    {
+        try {
+            $this->filesystem->delete($newpath);
+        } catch (FileNotFoundException $e) {
+            // The destination path does not exist. That's ok.
+        }
+
+        return $this->filesystem->copy($path, $newpath);
+    }
+}
---
+++ src/Plugin/ForcedRename.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace League\Flysystem\Plugin;
+
+use League\Flysystem\FileNotFoundException;
+
+class ForcedRename extends AbstractPlugin
+{
+    /**
+     * @inheritdoc
+     */
+    public function getMethod()
+    {
+        return 'forceRename';
+    }
+
+    /**
+     * Renames a file, overwriting the destination if it exists.
+     *
+     * @param string $path    Path to the existing file.
+     * @param string $newpath The new path of the file.
+     *
+     * @throws FileNotFoundException Thrown if $path does not exist.
+     *
+     * @return bool True on success, false on failure.
+     */
+    public function handle($path, $newpath)
+    {
+        try {
+            $this->filesystem->delete($newpath);
+        } catch (FileNotFoundException $e) {
+            // The destination path does not exist. That's ok.
+        }
+
+        return $this->filesystem->rename($path, $newpath);
+    }
+}
