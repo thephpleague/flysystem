550d1a4f966bae6365010c9c3f07879cd3b3bac6
BEAR <RubtsovAV@gmail.com>
2016-10-10 06:00:19 +0300

    Fix Utils::normalizePath regexp
    Fix regexp which chek normalized path on outside ot the defined root.

--- src/Util.php
+++ src/Util.php
@@ -85,7 +85,7 @@ class Util
         $normalized = preg_replace('#\p{C}+|^\./#u', '', $path);
         $normalized = static::normalizeRelativePath($normalized);
 
-        if (preg_match('#/\.{2}|^\.{2}/|^\.{2}$#', $normalized)) {
+        if (preg_match('#(^|/)\.{2}(/|$)#', $normalized)) {
             throw new LogicException(
                 'Path is outside of the defined root, path: [' . $path . '], resolved: [' . $normalized . ']'
             );
