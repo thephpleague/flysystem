99a8d7b91a72fc7b49ad37207f70cd71c752cea4
James Thomin <james.thomin@gmail.com>
2018-04-23 16:00:51 -0500

    Added some mimetypes to list

--- src/Util/MimeType.php
+++ src/Util/MimeType.php
@@ -165,6 +165,7 @@ class MimeType
             'movie' => 'video/x-sgi-movie',
             'doc'   => 'application/msword',
             'docx'  => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            'docm'  => 'application/vnd.ms-word.template.macroEnabled.12',
             'dot'   => 'application/msword',
             'dotx'  => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
             'xlsx'  => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
@@ -214,6 +215,18 @@ class MimeType
             'cdr'   => 'application/cdr',
             'wma'   => 'audio/x-ms-wma',
             'jar'   => 'application/java-archive',
+            'tex'   => 'application/x-tex',
+            'latex' => 'application/x-latex',
+            'odt'   => 'application/vnd.oasis.opendocument.text',
+            'ods'   => 'application/vnd.oasis.opendocument.spreadsheet',
+            'odp'   => 'application/vnd.oasis.opendocument.presentation',
+            'odg'   => 'application/vnd.oasis.opendocument.graphics',
+            'odc'   => 'application/vnd.oasis.opendocument.chart',
+            'odf'   => 'application/vnd.oasis.opendocument.formula',
+            'odi'   => 'application/vnd.oasis.opendocument.image',
+            'odm'   => 'application/vnd.oasis.opendocument.text-master',
+            'odb'   => 'application/vnd.oasis.opendocument.database',
+            'ott'   => 'application/vnd.oasis.opendocument.text-template',
         ];
     }
 }
