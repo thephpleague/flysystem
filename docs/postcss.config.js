224add7a5dad79fafd0dd89edaede89c3f7af20e
Frank de Jonge <info@frenky.net>
2018-04-25 17:13:52 +0200

    Use csso the other plugin re-ordered css which broke the design.

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
-        require("postcss-import"),
-        require('tailwindcss')('./tailwind.js')
+        require('postcss-import'),
+        require('tailwindcss')('./tailwind.js'),
+        require('postcss-csso')
     ]
 };
