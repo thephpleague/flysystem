8518a6983b70c647d74921b0a3db96d40a5d2755
Frank de Jonge <info@frenky.net>
2019-12-27 23:06:08 +0100

    ImMemory visibily and stream reading tests

--- src/InMemory/InMemoryFilesystemTest.php
+++ src/InMemory/InMemoryFilesystemTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace League\Flysystem\InMemory;
 
 use League\Flysystem\Config;
+use League\Flysystem\UnableToReadFile;
+use League\Flysystem\Visibility;
 use PHPUnit\Framework\TestCase;
 
 class InMemoryFilesystemTest extends TestCase
@@ -29,6 +31,81 @@ class InMemoryFilesystemTest extends TestCase
         $this->adapter->write(self::PATH, 'contents', new Config());
         $contents = $this->adapter->read(self::PATH);
         $this->assertEquals('contents', $contents);
+    }
+
+    /**
+     * @test
+     */
+    public function setting_visibility()
+    {
+        $this->adapter->write(self::PATH, 'contents', new Config(['visibility' => Visibility::PRIVATE]));
+        $contents = $this->adapter->visibility(self::PATH);
+        $this->assertEquals(Visibility::PRIVATE, $contents);
+        $this->adapter->setVisibility(self::PATH, Visibility::PUBLIC);
+        $contents = $this->adapter->visibility(self::PATH);
+        $this->assertEquals(Visibility::PUBLIC, $contents);
+    }
+
+    /**
+     * @test
+     */
+    public function updating_and_reading_a_file()
+    {
+        $this->adapter->update(self::PATH, 'contents', new Config());
+        $contents = $this->adapter->read(self::PATH);
+        $this->assertEquals('contents', $contents);
+    }
+
+    /**
+     * @test
+     */
+    public function writing_with_a_stream_and_reading_a_file()
+    {
+        $handle = stream_with_contents('contents');
+        $this->adapter->writeStream(self::PATH, $handle, new Config());
+        $contents = $this->adapter->read(self::PATH);
+        $this->assertEquals('contents', $contents);
+    }
+
+    /**
+     * @test
+     */
+    public function updating_with_a_stream_and_reading_a_file()
+    {
+        $handle = stream_with_contents('contents');
+        $this->adapter->updateStream(self::PATH, $handle, new Config());
+        fclose($handle);
+        $contents = $this->adapter->read(self::PATH);
+        $this->assertEquals('contents', $contents);
+    }
+
+    /**
+     * @test
+     */
+    public function reading_a_stream()
+    {
+        $this->adapter->write(self::PATH, 'contents', new Config());
+        $contents = $this->adapter->readStream(self::PATH);
+        $this->assertEquals('contents', stream_get_contents($contents));
+        fclose($contents);
+    }
+
+    /**
+     * @test
+     */
+    public function reading_a_non_existing_file()
+    {
+        $this->expectException(UnableToReadFile::class);
+        $this->adapter->read('path.txt');
+    }
+
+    /**
+     * @test
+     */
+    public function stream_reading_a_non_existing_file()
+    {
+        $this->expectException(UnableToReadFile::class);
+        $this->adapter->readStream('path.txt');
     }
 
     /**
