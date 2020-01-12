9b83d20355b14bdc5523fd8eb4d1fcd2e7159e4a
Frank de Jonge <info@frenky.net>
2020-01-12 22:54:59 +0100

    Added AWS S3 tests

---
+++ src/AwsS3V3/AwsS3V3FilesystemTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace League\Flysystem\AwsS3V3;
+
+use Aws\S3\S3Client;
+use Aws\S3\S3ClientInterface;
+use League\Flysystem\Config;
+use League\Flysystem\StorageAttributes;
+use PHPUnit\Framework\TestCase;
+
+class AwsS3V3FilesystemTest extends TestCase
+{
+    private $shouldCleanUp = false;
+
+    protected function tearDown(): void
+    {
+        if ( ! $this->shouldCleanUp) {
+            return;
+        }
+
+        $adapter = $this->adapter();
+        /** @var StorageAttributes[] $listing */
+        $listing = $adapter->listContents('', false);
+
+        foreach ($listing as $item) {
+            if ($item->isFile()) {
+                $adapter->delete($item->path());
+            } else {
+                $adapter->deleteDirectory($item->path());
+            }
+        }
+    }
+
+    private function s3Client(): S3ClientInterface
+    {
+        $key = getenv('FLYSYSTEM_AWS_S3_KEY');
+        $secret = getenv('FLYSYSTEM_AWS_S3_SECRET');
+        $bucket = getenv('FLYSYSTEM_AWS_S3_BUCKET');
+        $region = getenv('FLYSYSTEM_AWS_S3_REGION') ?: 'eu-central-1';
+
+        if ( ! $key || ! $secret || ! $bucket) {
+            $this->markTestSkipped('No AWS credentials present for testing.');
+        }
+
+        $this->shouldCleanUp = true;
+        $options = ['version' => 'latest', 'credentials' => compact('key', 'secret'), 'region' => $region];
+
+        return new S3Client($options);
+    }
+
+    private function adapter(): AwsS3V3Filesystem
+    {
+        $client = $this->s3Client();
+        $bucket = getenv('FLYSYSTEM_AWS_S3_BUCKET');
+        $prefix = getenv('FLYSYSTEM_AWS_S3_PREFIX') ?: '';
+
+        return new AwsS3V3Filesystem($client, $bucket, $prefix);
+    }
+
+    /**
+     * @test
+     */
+    public function writing_and_reading()
+    {
+        $adapter = $this->adapter();
+        $adapter->write('some/path.txt', 'contents', new Config());
+        $contents = $adapter->read('some/path.txt');
+        $this->assertEquals('contents', $contents);
+    }
+
+    /**
+     * @test
+     */
+    public function checking_if_files_exist()
+    {
+        $adapter = $this->adapter();
+        $this->assertFalse($adapter->fileExists('some/path.txt'));
+        $adapter->write('some/path.txt', 'contents', new Config());
+        $this->assertTrue($adapter->fileExists('some/path.txt'));
+    }
+}
