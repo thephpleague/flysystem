0d2e386521b468adcf843f1d1733da7eff745eb4
Frank de Jonge <info@frenky.net>
2020-07-24 06:58:33 +0200

    Catch type-error for FTP connections on PHP 8

--- src/FTP/NoopCommandConnectivityChecker.php
+++ src/FTP/NoopCommandConnectivityChecker.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace League\Flysystem\FTP;
 
+use TypeError;
+
 class NoopCommandConnectivityChecker implements ConnectivityChecker
 {
     public function isConnected($connection): bool
     {
-        $response = @ftp_raw($connection, 'NOOP');
+        try {
+            $response = @ftp_raw($connection, 'NOOP');
+        } catch (TypeError $typeError) {
+            return false;
+        }
+
         $responseCode = $response ? (int) preg_replace('/\D/', '', implode('', $response)) : false;
 
         return $responseCode === 200;
