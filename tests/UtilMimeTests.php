4e9ad8fd704fac861f614d923e8dd7dfebd27738
Frank de Jonge <info@frenky.net>
2020-08-08 16:04:45 +0200

    Remove finfo existence checks, it's a required dependency.

--- tests/UtilMimeTests.php
+++ tests/UtilMimeTests.php
@@ -4,31 +4,8 @@ namespace League\Flysystem\Util;
 
 use PHPUnit\Framework\TestCase;
 
-$passthru = true;
-
-function class_exists($class_name, $autoload = true)
-{
-    global $passthru;
-
-    if ($passthru) {
-        return \class_exists($class_name, $autoload);
-    }
-
-    return false;
-}
-
 class UtilMimeTests extends TestCase
 {
-    // use \PHPUnitHacks;
-
-    public function testNoFinfoFallback()
-    {
-        global $passthru;
-        $passthru = false;
-        $this->assertNull(MimeType::detectByContent('string'));
-        $passthru = true;
-    }
-
     public function testRetrievingAllMimetypes()
     {
         $map = MimeType::getExtensionToMimeTypeMap();
