a78759e1cb948c13981cc1572a118291a1d1d916
Frank de Jonge <info@frenky.net>
2020-10-17 12:54:44 +0200

    Fixed CS

--- src/AdapterTestUtilities/RetryOnTestException.php
+++ src/AdapterTestUtilities/RetryOnTestException.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace League\Flysystem\AdapterTestUtilities;
 
-use PHPUnit\Framework\ExpectationFailedException;
 use const PHP_EOL;
 use const STDOUT;
 use Throwable;
