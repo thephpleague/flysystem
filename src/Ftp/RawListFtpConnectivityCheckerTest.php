2f02a97fd103c1c69021410b9a1a2064d90f4c03
Frank de Jonge <info@frenky.net>
2020-11-07 20:28:49 +0100

    Run scenario with retries

--- src/Ftp/RawListFtpConnectivityCheckerTest.php
+++ src/Ftp/RawListFtpConnectivityCheckerTest.php
@@ -2,6 +2,7 @@
 
 namespace League\Flysystem\Ftp;
 
+use League\Flysystem\AdapterTestUtilities\RetryOnTestException;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -9,28 +10,28 @@ use PHPUnit\Framework\TestCase;
  */
 class RawListFtpConnectivityCheckerTest extends TestCase
 {
+    use RetryOnTestException;
     /**
      * @test
      */
     public function detecting_if_a_connection_is_connected(): void
     {
-        $options = FtpConnectionOptions::fromArray([
-           'host' => 'localhost',
-           'port' => 2121,
-           'root' => '/home/foo/upload/',
-           'username' => 'foo',
-           'password' => 'pass',
-       ]);
+        $this->retryOnException(UnableToConnectToFtpHost::class);
+        $this->runScenario(function() {
+            $options = FtpConnectionOptions::fromArray([
+               'host' => 'localhost',
+               'port' => 2121,
+               'root' => '/home/foo/upload/',
+               'username' => 'foo',
+               'password' => 'pass',
+           ]);
 
-        $provider = new FtpConnectionProvider();
-        $connection = $provider->createConnection($options);
-
-        $connectedChecker = new RawListFtpConnectivityChecker();
-
-        $this->assertTrue($connectedChecker->isConnected($connection));
-
-        @ftp_close($connection);
-
-        $this->assertFalse($connectedChecker->isConnected($connection));
+            $provider = new FtpConnectionProvider();
+            $connection = $provider->createConnection($options);
+            $connectedChecker = new RawListFtpConnectivityChecker();
+            $this->assertTrue($connectedChecker->isConnected($connection));
+            @ftp_close($connection);
+            $this->assertFalse($connectedChecker->isConnected($connection));
+        });
     }
 }
