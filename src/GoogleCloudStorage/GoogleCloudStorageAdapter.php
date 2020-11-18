60ad6ae7068b385f384144c04d19623ea86c5030
Frank de Jonge <info@frenky.net>
2020-11-18 22:09:58 +0100

    CS

--- src/GoogleCloudStorage/GoogleCloudStorageAdapter.php
+++ src/GoogleCloudStorage/GoogleCloudStorageAdapter.php
@@ -5,27 +5,20 @@ declare(strict_types=1);
 namespace League\Flysystem\GoogleCloudStorage;
 
 use Google\Cloud\Core\Exception\NotFoundException;
-use Google\Cloud\Storage\Acl;
 use Google\Cloud\Storage\Bucket;
-use Google\Cloud\Storage\ObjectIterator;
 use Google\Cloud\Storage\StorageObject;
 use League\Flysystem\Config;
 use League\Flysystem\DirectoryAttributes;
 use League\Flysystem\FileAttributes;
 use League\Flysystem\FilesystemAdapter;
-use League\Flysystem\FilesystemException;
-use League\Flysystem\InvalidVisibilityProvided;
 use League\Flysystem\PathPrefixer;
 use League\Flysystem\StorageAttributes;
 use League\Flysystem\UnableToCopyFile;
-use League\Flysystem\UnableToCreateDirectory;
-use League\Flysystem\UnableToDeleteDirectory;
 use League\Flysystem\UnableToDeleteFile;
 use League\Flysystem\UnableToMoveFile;
 use League\Flysystem\UnableToReadFile;
 use League\Flysystem\UnableToRetrieveMetadata;
 use League\Flysystem\UnableToSetVisibility;
-use League\Flysystem\UnableToWriteFile;
 use League\Flysystem\Visibility;
 use Throwable;
 
