7841ba623a080ba17fd2b0f7eb39296bbd4db296
Frank de Jonge <info@frenky.net>
2020-11-22 21:24:04 +0100

    Implemented failure scenarios

--- bin/push.sh
+++ bin/push.sh
@@ -29,6 +29,7 @@ remote memory git@github.com:thephpleague/flysystem-memory.git
 remote ziparchive git@github.com:thephpleague/flysystem-ziparchive.git
 remote aws-s3-v3 git@github.com:thephpleague/flysystem-aws-s3-v3.git
 remote async-aws-s3 git@github.com:thephpleague/flysystem-async-aws-s3.git
+remote google-cloud-storage git@github.com:thephpleague/flysystem-google-cloud-storage.git
 remote adapter-test-utilities git@github.com:thephpleague/flysystem-adapter-test-utilities.git
 
 split 'src/Ftp' ftp
@@ -37,4 +38,5 @@ split 'src/InMemory' memory
 split 'src/ZipArchive' ziparchive
 split 'src/AwsS3V3' aws-s3-v3
 split 'src/AsyncAwsS3' async-aws-s3
+split 'src/GoogleCloudStorage' google-cloud-storage
 split 'src/AdapterTestUtilities' adapter-test-utilities
--- src/GoogleCloudStorage/GoogleCloudStorageAdapterTest.php
+++ src/GoogleCloudStorage/GoogleCloudStorageAdapterTest.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace League\Flysystem\GoogleCloudStorage;
 
-use Google\Cloud\Storage\StorageClient;
 use League\Flysystem\AdapterTestUtilities\FilesystemAdapterTestCase;
+use League\Flysystem\Config;
 use League\Flysystem\FilesystemAdapter;
+use League\Flysystem\UnableToDeleteDirectory;
+use League\Flysystem\UnableToDeleteFile;
+use League\Flysystem\UnableToRetrieveMetadata;
+use League\Flysystem\UnableToWriteFile;
 
 /**
  * @group gcs
@@ -17,6 +21,11 @@ class GoogleCloudStorageAdapterTest extends FilesystemAdapterTestCase
      * @var string
      */
     private static $adapterPrefix = 'ci';
+
+    /**
+     * @var StubBucket
+     */
+    private static $bucket;
 
     public static function setUpBeforeClass(): void
     {
@@ -32,10 +41,98 @@ class GoogleCloudStorageAdapterTest extends FilesystemAdapterTestCase
         $clientOptions = [
             'projectId' => 'flysystem-testing',
             'keyFilePath' => __DIR__ . '/../../google-cloud-service-account.json',
+
         ];
-        $storageClient = new StorageClient($clientOptions);
-        $bucket = $storageClient->bucket('flysystem');
+        $storageClient = new StubStorageClient($clientOptions);
+        $connection = $storageClient->connection();
+        $projectId = $storageClient->projectId();
+
+        static::$bucket = $bucket = new StubBucket($connection, 'flysystem', [
+            'requesterProjectId' => $projectId,
+        ]);
 
         return new GoogleCloudStorageAdapter($bucket, static::$adapterPrefix);
+    }
+
+    /**
+     * @test
+     */
+    public function fetching_visibility_of_non_existing_file(): void
+    {
+        $this->markTestSkipped("
+            Not relevant for this adapter since it's a missing ACL,
+            which turns into a 404 which is the expected outcome
+            of a private visibility. 🤷‍♂️
+        ");
+    }
+
+    /**
+     * @test
+     */
+    public function fetching_unknown_mime_type_of_a_file(): void
+    {
+        $this->markTestSkipped("This adapter always returns a mime-type.");
+    }
+
+    /**
+     * @test
+     */
+    public function listing_a_toplevel_directory(): void
+    {
+        $this->clearStorage();
+        parent::listing_a_toplevel_directory();
+    }
+
+    /**
+     * @test
+     */
+    public function failing_to_write_a_file(): void
+    {
+        $adapter = $this->adapter();
+        static::$bucket->failOnUpload();
+
+        $this->expectException(UnableToWriteFile::class);
+
+        $adapter->write('something.txt', 'contents', new Config());
+    }
+
+    /**
+     * @test
+     */
+    public function failing_to_delete_a_file(): void
+    {
+        $adapter = $this->adapter();
+        static::$bucket->withObject(static::$adapterPrefix.'/filename.txt')->failWhenDeleting();
+
+        $this->expectException(UnableToDeleteFile::class);
+
+        $adapter->delete('filename.txt');
+    }
+
+    /**
+     * @test
+     */
+    public function failing_to_delete_a_directory(): void
+    {
+        $adapter = $this->adapter();
+        $this->givenWeHaveAnExistingFile('dir/filename.txt');
+        static::$bucket->withObject(static::$adapterPrefix.'/dir/filename.txt')->failWhenDeleting();
+
+        $this->expectException(UnableToDeleteDirectory::class);
+
+        $adapter->deleteDirectory('dir');
+    }
+
+    /**
+     * @test
+     */
+    public function failing_to_retrieve_visibility(): void
+    {
+        $adapter = $this->adapter();
+        static::$bucket->withObject(static::$adapterPrefix.'/filename.txt')->failWhenAccessingAcl();
+
+        $this->expectException(UnableToRetrieveMetadata::class);
+
+        $adapter->visibility('filename.txt');
     }
 }
