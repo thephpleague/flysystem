1e9ca1099a007b51f94a5be41c7108752846a8f7
Frank de Jonge <info@frankdejonge.nl>
2022-07-03 12:43:23 +0200

    Test sub-split closer on AwsS3V3

---
+++ bin/update-subsplit-closers.php
@@ -0,0 +1,19 @@
+<?php
+
+use League\Flysystem\Filesystem;
+use League\Flysystem\Local\LocalFilesystemAdapter;
+
+include __DIR__ . '/../vendor/autoload.php';
+
+$filesystem = new Filesystem(new LocalFilesystemAdapter(realpath(__DIR__ . '/../')));
+$subsplits = json_decode($filesystem->read('config.subsplit-publish.json'), true);
+$workflowContents = $filesystem->read('bin/close-subsplit-prs.yml');
+
+foreach ($subsplits['sub-splits'] as ['directory' => $subsplit]) {
+    if ($subsplit !== 'src/AwsS3V3') {
+        continue;
+    }
+
+    $workflowPath = $subsplit . '/.github/workflows/close-subsplit-prs.yaml';
+    $filesystem->write($workflowPath, $workflowContents);
+}
