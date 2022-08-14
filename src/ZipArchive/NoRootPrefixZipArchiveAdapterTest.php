99682a34326862fb89b6ae6d80f93b0e357d9139
Frank de Jonge <info@frankdejonge.nl>
2022-08-14 22:46:54 +0200

    Put zip tests in zip group

--- src/ZipArchive/NoRootPrefixZipArchiveAdapterTest.php
+++ src/ZipArchive/NoRootPrefixZipArchiveAdapterTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace League\Flysystem\ZipArchive;
 
+/**
+ * @group zip
+ */
 final class NoRootPrefixZipArchiveAdapterTest extends ZipArchiveAdapterTest
 {
     protected static function getRoot(): string
--- src/ZipArchive/PrefixedRootZipArchiveAdapterTest.php
+++ src/ZipArchive/PrefixedRootZipArchiveAdapterTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace League\Flysystem\ZipArchive;
 
+/**
+ * @group zip
+ */
 final class PrefixedRootZipArchiveAdapterTest extends ZipArchiveAdapterTest
 {
     protected static function getRoot(): string
